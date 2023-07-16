ef5c22fbaa7332843f2031204b7e5b709c210600
Efen Stakes <efenstakes101@gmail.com>
2023-07-16 15:37:11 +0300

    add driver components

---
+++ src/components/furious_driver/component.tsx
@@ -0,0 +1,119 @@
+import * as THREE from "three";
+import React, { useEffect, useRef } from "react";
+import { useGLTF, useAnimations } from "@react-three/drei";
+import { GLTF } from "three-stdlib";
+
+type GLTFResult = GLTF & {
+  nodes: {
+    EyeLeft: THREE.SkinnedMesh;
+    EyeRight: THREE.SkinnedMesh;
+    Wolf3D_Hair: THREE.SkinnedMesh;
+    Wolf3D_Head: THREE.SkinnedMesh;
+    Wolf3D_Outfit_Bottom: THREE.SkinnedMesh;
+    Wolf3D_Outfit_Footwear: THREE.SkinnedMesh;
+    Wolf3D_Outfit_Top: THREE.SkinnedMesh;
+    Wolf3D_Teeth: THREE.SkinnedMesh;
+    Hips: THREE.Bone;
+  };
+  materials: {
+    ["Wolf3D_Eye.015"]: THREE.MeshStandardMaterial;
+    ["Wolf3D_Hair.011"]: THREE.MeshStandardMaterial;
+    ["Wolf3D_Skin.015"]: THREE.MeshStandardMaterial;
+    ["Wolf3D_Outfit_Bottom.011"]: THREE.MeshStandardMaterial;
+    ["Wolf3D_Outfit_Footwear.015"]: THREE.MeshStandardMaterial;
+    ["Wolf3D_Outfit_Top.015"]: THREE.MeshStandardMaterial;
+    ["Wolf3D_Teeth.015"]: THREE.MeshStandardMaterial;
+  };
+};
+
+type ActionName =
+  | "Armature|mixamo.com|Layer0"
+  | "CatWalk"
+  | "Idle"
+  | "Idle Happy"
+  | "Look Around"
+  | "Warrior";
+type GLTFActions = Record<ActionName, THREE.AnimationAction>;
+
+export function FuriousDriverModel(props: JSX.IntrinsicElements["group"]) {
+    const group = useRef<THREE.Group>();
+    const { nodes, materials, animations } = useGLTF(
+        "/models/furious-driver.glb"
+    ) as GLTFResult;
+    const { actions } = useAnimations(animations, group);
+    
+    console.log("actions ", actions);
+    
+
+    useEffect(()=> {
+        actions['Warrior']?.play()
+    }, [])
+
+    return (
+        <group ref={group} {...props} dispose={null}>
+        <group name="Scene">
+            <group name="Armature">
+            <skinnedMesh
+                name="EyeLeft"
+                geometry={nodes.EyeLeft.geometry}
+                material={materials["Wolf3D_Eye.015"]}
+                skeleton={nodes.EyeLeft.skeleton}
+                morphTargetDictionary={nodes.EyeLeft.morphTargetDictionary}
+                morphTargetInfluences={nodes.EyeLeft.morphTargetInfluences}
+            />
+            <skinnedMesh
+                name="EyeRight"
+                geometry={nodes.EyeRight.geometry}
+                material={materials["Wolf3D_Eye.015"]}
+                skeleton={nodes.EyeRight.skeleton}
+                morphTargetDictionary={nodes.EyeRight.morphTargetDictionary}
+                morphTargetInfluences={nodes.EyeRight.morphTargetInfluences}
+            />
+            <skinnedMesh
+                name="Wolf3D_Hair"
+                geometry={nodes.Wolf3D_Hair.geometry}
+                material={materials["Wolf3D_Hair.011"]}
+                skeleton={nodes.Wolf3D_Hair.skeleton}
+            />
+            <skinnedMesh
+                name="Wolf3D_Head"
+                geometry={nodes.Wolf3D_Head.geometry}
+                material={materials["Wolf3D_Skin.015"]}
+                skeleton={nodes.Wolf3D_Head.skeleton}
+                morphTargetDictionary={nodes.Wolf3D_Head.morphTargetDictionary}
+                morphTargetInfluences={nodes.Wolf3D_Head.morphTargetInfluences}
+            />
+            <skinnedMesh
+                name="Wolf3D_Outfit_Bottom"
+                geometry={nodes.Wolf3D_Outfit_Bottom.geometry}
+                material={materials["Wolf3D_Outfit_Bottom.011"]}
+                skeleton={nodes.Wolf3D_Outfit_Bottom.skeleton}
+            />
+            <skinnedMesh
+                name="Wolf3D_Outfit_Footwear"
+                geometry={nodes.Wolf3D_Outfit_Footwear.geometry}
+                material={materials["Wolf3D_Outfit_Footwear.015"]}
+                skeleton={nodes.Wolf3D_Outfit_Footwear.skeleton}
+            />
+            <skinnedMesh
+                name="Wolf3D_Outfit_Top"
+                geometry={nodes.Wolf3D_Outfit_Top.geometry}
+                material={materials["Wolf3D_Outfit_Top.015"]}
+                skeleton={nodes.Wolf3D_Outfit_Top.skeleton}
+            />
+            <skinnedMesh
+                name="Wolf3D_Teeth"
+                geometry={nodes.Wolf3D_Teeth.geometry}
+                material={materials["Wolf3D_Teeth.015"]}
+                skeleton={nodes.Wolf3D_Teeth.skeleton}
+                morphTargetDictionary={nodes.Wolf3D_Teeth.morphTargetDictionary}
+                morphTargetInfluences={nodes.Wolf3D_Teeth.morphTargetInfluences}
+            />
+            <primitive object={nodes.Hips} />
+            </group>
+        </group>
+        </group>
+    );
+}
+
+useGLTF.preload("/models/furious-driver.glb");
